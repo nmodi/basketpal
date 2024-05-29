992346cf767dca087c863372cd0e2eaf4cc24d26
Nilay Modi <nilaymodi@Nilays-M1-MBP.local>
2024-05-28 20:19:05 -0500

    add port

--- basketpal-backend/main.py
+++ basketpal-backend/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from nba_service import *
 
@@ -17,4 +19,9 @@ async def get_boxscore(game_id: str):
 @app.get("/games/{game_id}/playbyplay")
 async def get_playbyplay(game_id: str):
     return fetch_playbyplay(game_id)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
